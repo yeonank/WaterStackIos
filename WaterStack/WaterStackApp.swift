81fec73a15f8eaa80407cc1cbdcbc98b8235b804
yeonank <yeonank99@gmail.com>
2026-03-13 22:51:40 +0900

    Initial Commit

---
+++ WaterStack/WaterStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterStackApp.swift
+//  WaterStack
+//
+//  Created by 임연정 on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct WaterStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaterStackTests/WaterStackTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaterStackTests.swift
+//  WaterStackTests
+//
+//  Created by 임연정 on 3/13/26.
+//
+
+import Testing
+@testable import WaterStack
+
+struct WaterStackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
